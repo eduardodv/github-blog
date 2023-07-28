834816aafc8a1992e1daa97a2062a4bb0fba65f2
Eduardo Dalla Vecchia <eduardo@MacBook-Pro-de-Eduardo.local>
2023-07-28 15:55:55 -0300

    Initial configs

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,25 @@
+export const defaultTheme = {
+  colors: {
+    white: '#fff',
+    'blue-500': '#3294F8',
+
+    'base-title': '#E7EDF4',
+    'base-subtitle': '#C4D4E3',
+    'base-text': '#AFC2D4',
+    'base-span': '#7B96B2',
+    'base-label': '#3A536B',
+    'base-border': '#1C2F41',
+    'base-post': '#112131',
+    'base-profile': '#0B1B2B',
+    'base-background': '#071422',
+    'base-input': '#040F1A',
+  },
+  sizes: {
+    'text-xs': '.75rem',
+    'text-md': '.875rem',
+    'text-lg': '1rem',
+    'text-xl': '1.125rem',
+    'title-md': '1.25rem',
+    'title-lg': '1.5rem',
+  },
+} as const
